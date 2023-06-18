72cd4d44d1e48fae7aac1a430015ea5d66886f91
Subat Ahsan <subat.ahsan18@gmail.com>
2023-06-18 06:40:50 -0400

    Pause,play, fullscreen and file input for player

--- server.py
+++ server.py
@@ -1,23 +1,82 @@
 #!/usr/bin/env python3
 import time
 import mpv
+from os import listdir
+from os.path import exists
+import pymkv
 
-player = mpv.MPV()
+DIR="videos"
 
-player.loop = True
-player.play('1.mp4')
+def swap(value):
+    if(value == False):
+        return True
+    return False
 
+
+player = mpv.MPV(fs=False)
+player.window_maximized=True
+while True:
+    fn = input("Enter filename (q to quit): ")
+    if (fn == 'q'):
+        exit()
+    print(fn)
+    if (not (exists(f"./{DIR}/{fn}"))):
+        print("File not found")
+        continue
+    fileType=fn.split(".")[-1]
+    if ( fileType not in {'mp4', 'mkv', 'mp3'}):
+        print("Invalid type")
+        continue
+    break
+
+if (fileType == "mkv"):
+    pass
+
+player.play(f'./{DIR}/{fn}')
 player.wait_until_playing()
 
+audioTrack=1
+VideoTrack=1
+
+if (fileType=="mkv"):
+    player.pause=True
+    print("\nAudio Tracks:")
+
+    for i in player.track_list:
+        if (i['type'] == 'audio'):
+            if ('title' in i.keys()):
+                print(f"{i['id']}. {i['title']} ({i['lang']})")
+            else:
+                print(f"{i['id']}. ({i['lang']})")
+
+    audioTrack = int(input("\nSelect audio track: "))
+    
+    print("\nSub Tracks:")
+    for i in player.track_list:
+        if (i['type'] == 'sub'):
+            if ('title' in i.keys()):
+                print(f"{i['id']}. {i['title']} ({i['lang']})")
+            else:
+                print(f"{i['id']}. ({i['lang']})")
+
+    subTrack = int(input("\nSelect sub track: "))
+    player.aid=audioTrack
+    player.sid=subTrack
+    print("Hit play")
+
+@player.on_key_press('f')
+def my_q_binding():
+    player.fs = swap(player.fs)
+
+@player.on_key_press('space')
+def my_q_binding():
+    player.pause = swap(player.pause)
+ 
 
 
+player.show_text("hello", 1000)
 while not player._core_shutdown:
-   x = input()
-   if (x == 'p'):
-      if (player._get_property("pause") == True):
-         player._set_property("pause", False)
-         continue
-      player._set_property("pause", True)
-   else:
-      continue
    
+    continue
+
+
